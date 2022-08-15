3af00661fa4744f64eab579c92fe8df0bf2ff3a9
SARMADSPC\sarma <sarmad9@hotmail.de>
2022-08-15 12:55:48 +0200

    add template, add first home lines, install ts

---
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -0,0 +1,45 @@
+import Typical from "react-typical";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://www.facebook.com/profile.php?id=100008128277131">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="https://www.instagram.com/_sarmad.ah_/?hl=de">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="https://www.youtube.com/channel/UCbeERm_hcKzShLDdMuBIhxQ">
+              <i className="fa fa-youtube-square"></i>
+            </a>
+            <a href="https://twitter.com/Sarmad20962116">
+              <i className="fa fa-twitter"></i>
+            </a>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              Hello, I'm <span className="highlighted-text">Sarmad</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              <h1>
+                <Typical loop={Infinity} steps={["Aspiring Developer 🌱", 1000, "Front End Developer 👨‍💻", 1000, "Software Expert", 1000]} />
+              </h1>
+              <span className="profile-role-tagline">Exploring the world of web development</span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">Hire Me</button>
+          </div>
+          <a href="CV Sarmad Ahmad.pdf" download="CV Sarmad Ahmad">
+            <button className="btn highlighted-btn">Get Resume</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
