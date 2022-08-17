ea4057f03283c550eed1513572d23b4b03dda425
SARMADSPC\sarma <sarmad9@hotmail.de>
2022-08-17 19:58:34 +0200

    add nav, add responsive nav

--- src/PortfolioContainer/Home/Home.tsx
+++ src/PortfolioContainer/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer/Footer";
 import Profile from "../Home/Profile/Profile";
 import "./Home.css";
+import Header from "./Header/Header";
 
 export default function Home() {
   return (
     <div className="home-container">
+      <Header />
       <Profile />
       <Footer />
     </div>
